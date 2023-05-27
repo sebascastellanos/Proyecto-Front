35e640196c4b76b810dc61089a612e652797fb11
sarapiraquive <sarapirpa@unisabana.edu.co>
2023-05-27 18:36:14 -0500

    Agegar y cargar libros

--- biblioteca/src/componentes/TablaLibros.jsx
+++ biblioteca/src/componentes/TablaLibros.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export const TablaEstudiante = ({ listaEstudiantes }) => {
+export const TablaLibros = ({ listaLibros }) => {
 
  
-  const [buscarFac, setBuscarFac] = useState("");
+  const [buscarGen, setBuscarGen] = useState("");
   
-  const [estudiantesEncontradosFac, setEstudiantesEncontradosFac] = useState([]);
+  const [LibrosEncontradosGen, setLibrosEncontradosGen] = useState([]);
 
   const editar = (event) => {
     
@@ -18,7 +18,7 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
     const idCell = row.querySelector('td:first-child');//toma la primera data de el respectivo row lo que seria el id
     const id = idCell.textContent.trim();
 
-    fetch(`http://localhost:8080/estudiante/eliminar/${id}`, {
+    fetch(`http://localhost:8080/libro/eliminar/${id}`, {
       method: 'DELETE',
     })
       .then(response => {
@@ -28,42 +28,43 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
       })
   }
 
-  const buscarEstudiantePorFacultad = (facultad) => {
-    return listaEstudiantes.filter((estudiante) => estudiante.facultad == facultad)
+  const buscarLibroPorGenero = (genero) => {
+    return listaLibros.filter((libro) => libro.genero == genero)
   }
 
   
 
-  const buscarEstFac = (event) => {
+  const buscarLibGen = (event) => {
     event.preventDefault();
-    const estudiantesEncontrados = buscarEstudiantePorFacultad(buscarFac);
-    setEstudiantesEncontradosFac(estudiantesEncontrados);
+    const LibrosEncontrados = buscarLibroPorGenero(buscarGen);
+    setLibrosEncontradosGen(LibrosEncontrados);
   };
 
  
 
   const limpiarBusqueda = () => {
     
-    setBuscarFac("");
+    setBuscarGen("");
    
-    setEstudiantesEncontradosFac([]);
+    setLibrosEncontradosGen([]);
   };
 
   return (
     <>
-      <form onSubmit={buscarEstFac}>
+      <form onSubmit={buscarLibGen}>
         <br />
         <div className="form-group input-group">
 
 
           <br></br>
-          <select ClassName="form-control" id="facultad" placeholder="facultad" value={buscarFac} onChange={(event) => setBuscarFac(event.target.value)}>
-            <option value="">--Seleccione la facultad--</option>
-            <option value="Ingenieria">Ingenieria</option>
-            <option value="Medicina">Medicina</option>
-            <option value="Comunicacion">Comunicacion</option>
-            <option value="Educacion">Educacion</option>
-            <option value="Derecho">Derecho</option>
+          <select ClassName="form-control" id="genero" placeholder="genero" value={buscarGen} onChange={(event) => setBuscarGen(event.target.value)}>
+            <option value="">--Seleccione el genero--</option>
+            <option value="Ficcion">Ficcion</option>
+            <option value="Novela">Novela</option>
+            <option value="Suspenso">Suspenso</option>
+            <option value="Fantasia">Fantasia</option>
+            <option value="Romance">Romance</option>
+            <option value="Historia">Historia</option>
           </select>
           <br></br>
           <button type="submit" className="btn btn-primary"> Buscar
@@ -72,28 +73,25 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
       </form>
 
       {
-        estudiantesEncontradosFac.length > 0 ? (
+        LibrosEncontradosGen.length > 0 ? (
          
           <div>
-            <h3>Estudiante encontrado:</h3>
+            <h3>Libro encontrado:</h3>
             <table className="table">
               <thead>
                 <tr>
-                  <th scope="col">Id Estudiante</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Semestre</th>
-                  <th scope="col">Facultad</th>
+                  <th scope="col">Id Libro</th>
+                  <th scope="col">Titulo</th>
+                  <th scope="col">Autor</th>
                   <th scope="col">Genero</th>
                 </tr>
               </thead>
               <tbody>
-                {estudiantesEncontradosFac.map((estudiante, index) => (
+                {LibrosEncontradosGen.map((libro, index) => (
                   <tr key={index}>
-                    <td>{estudiante.titulo}</td>
-                    <td>{estudiante.autor}</td>
-                    <td>{estudiante.semestre}</td>
-                    <td>{estudiante.facultad}</td>
-                    <td>{estudiante.genero}</td>
+                    <td>{libro.titulo}</td>
+                    <td>{libro.autor}</td>
+                    <td>{libro.genero}</td>
                     <td>
                     </td>
                   </tr>
@@ -108,23 +106,21 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
             <table className="table">
               <thead>
                 <tr>
-                  <th scope="col">Id Estudiante</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Semestre</th>
-                  <th scope="col">Facultad</th>
+                  <th scope="col">Id Libro</th>
+                  <th scope="col">Titulo</th>
+                  <th scope="col">Autor</th>
                   <th scope="col">Genero</th>
                   <th scope="col">Modificar</th>
                   <th scope="col">Eliminar</th>
                 </tr>
               </thead>
               <tbody>
-                {listaEstudiantes.map((estudiante) => (
-                  <tr key={estudiante.facultad}>
-                    <td>{estudiante.id}</td>
-                    <td>{estudiante.nombre}</td>
-                    <td>{estudiante.semestre}</td>
-                    <td>{estudiante.facultad}</td>
-                    <td>{estudiante.genero}</td>
+                {listaLibros.map((libro) => (
+                  <tr key={libro.genero}>
+                    <td>{libro.id}</td>
+                    <td>{libro.titulo}</td>
+                    <td>{libro.autor}</td>
+                    <td>{libro.genero}</td>
                     <td>
                       <button className="btn btn-success" onClick={editar}> Editar
                       </button>
